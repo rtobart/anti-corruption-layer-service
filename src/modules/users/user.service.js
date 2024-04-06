c34121bf9db38e95e5a68b2beff74edccfde0d7a
Robinson.tobar <robinson.tobar.ti@gmail.com>
2024-04-06 01:18:09 -0300

    restructure response

--- src/modules/users/user.service.js
+++ src/modules/users/user.service.js
@@ -6,8 +6,6 @@ export class UserService {
     async getUsers() {
         const docs = await firestore.collection('coleccion').get();
         const data = docs.docs.map(doc => doc.data());
-        return {
-            users: data.map(user => { return {name: user.userName} })
-        }
+        return data.map(user => { return {name: user.userName} })
     }
 }
